1097ad54a2bddfe46ef6068065123214151774fd
Javier Iniesta Fernández <jiniesta@encamina.com>
2019-04-16 10:30:59 +0200

    Add project files.

---
+++ GAB2019-ia-cybersecurity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GAB2019_ia_cybersecurity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
